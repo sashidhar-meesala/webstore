fce13a176c54259754073bc3e70acad67288a2b7
Sashidhar Meesala <sashidharklu@gmail.com>
2019-11-27 22:01:50 -0500

    second commit

--- js/index.js
+++ js/index.js
@@ -1,110 +1,450 @@
-//DATA 
+//DATA
 const nike1 = {
-    name: `Nike shoes 1`,
-    imageUrl:`img/product.jpg`,
-    alternateName:`shoes-alt-name`,
-    category:`casual`,
-    productCode: `WDDM-115`,
-    price:`100`,
-    rating: 4,
-    brand:`nike`,
-    description:`best shoes ever`,
-    colors:[`red`,`blue`,`balck`],
-    stock:10,
-    sizes:[7,8,9],
-    discount: 15,
-    onSale: true
-  };
-
-
-  const nike2 = {
-    name: `Nike shoes 2`,
-    imageUrl:`img/product.jpg`,
-    altname:`nikeshoe2`,
-    category:`sport`,
-    productCode: `WDDM-118`,
-    price:`110`,
-    rating: 3,
-    brand:`nike`,
-    description:`best shoes ever`,
-    colors:[`red`,`balck`],
-    stock:10,
-    sizes:[7,8,9],
-    discount: 20,
-    onSale: true
-  };
-
-
-  const nike3 = {
-    name: `Nike shoes 3`,
-    imageUrl:`img/product.jpg`,
-    productCode: `WDDM-117`,
-    altname:`nikeshoe3`,
-    category:`running`,
-    price:`100`,
-    rating: 2,
-    brand:`nike`,
-    description:`best shoes ever`,
-    colors:[`red`,`blue`],
-    stock:10,
-    sizes:[7,8,9],
-    discount: 5,
-    onSale: true
-  };
-
-
-const nike=[nike1,nike2,nike3];
-
-
-const puma1={
-    name: `puma shoes 1`,
-    imageUrl:`img/product.jpg`,
-    altname:`pumashoes1`,
-    category:`sport`,
-    productCode: `PUMA117`,
-    price:`1200`,
-    rating: 2,
-    brand:`nike`,
-    description:`best shoes ever`,
-    colors:[`red`,`blue`],
-    stock:10,
-    sizes:[7,8,9],
-    discount: 5,
-    onSale: true
+  name: `Nike shoes 1`,
+  imageUrl: `img/nike/casual/nike-casual%20(1).jpeg`,
+  alternateName: `shoes-alt-name`,
+  category: `casual`,
+  productCode: `NC100`,
+  price: `120`,
+  rating: 4.1,
+  brand: `nike`,
+  description: `best shoes ever`,
+  colors: [`red`, `blue`, `balck`],
+  stock: 10,
+  sizes: [7, 8, 9],
+  discount: 15,
+  onSale: true
 };
 
-
-const puma2={
-    name: `puma shoes 1`,
-    imageUrl:`img/product.jpg`,
-    productCode: `PUMA117`,
-    altname:`pumashoes2`,
-    category:`casual`,
-    price:`1200`,
-    rating: 2,
-    brand:`nike`,
-    description:`best shoes ever`,
-    colors:[`red`,`blue`],
-    stock:10,
-    sizes:[7,8,9],
-    discount: 5,
-    onSale: true
+const nike2 = {
+  name: `Nike shoes 2`,
+  imageUrl: `img/nike/casual/nike-casual%20(2).jpeg`,
+  altname: `nikeshoe2`,
+  category: `casual`,
+  productCode: `NC101`,
+  price: `120`,
+  rating: 3.2,
+  brand: `nike`,
+  description: `best shoes ever`,
+  colors: [`red`, `balck`],
+  stock: 10,
+  sizes: [7, 8, 9],
+  discount: 20,
+  onSale: true
 };
 
+const nike3 = {
+  name: `Nike shoes 3`,
+  imageUrl: `img/nike/casual/nike-casual%20(3).jpeg`,
+  productCode: `WDDM-117`,
+  altname: `nikeshoe3`,
+  category: `casual`,
+  price: `140`,
+  rating: 3.1,
+  brand: `nike`,
+  description: `best shoes ever`,
+  colors: [`red`, `blue`],
+  stock: 10,
+  sizes: [7, 8, 9],
+  discount: 5,
+  onSale: false
+};
 
-const puma=[puma1,puma2];
+const nike4 = {
+  name: `Nike shoes 3`,
+  imageUrl: `img/nike/casual/nike-casual%20(4).jpeg`,
+  productCode: `WDDM-117`,
+  altname: `nikeshoe3`,
+  category: `casual`,
+  price: `140`,
+  rating: 3.1,
+  brand: `nike`,
+  description: `best shoes ever`,
+  colors: [`red`, `blue`],
+  stock: 10,
+  sizes: [7, 8, 9],
+  discount: 5,
+  onSale: false
+};
 
+const nike5 = {
+  name: `Nike shoes 3`,
+  imageUrl: `img/nike/running/nike-running%20(1).jpeg`,
+  productCode: `WDDM-117`,
+  altname: `nikeshoe3`,
+  category: `casual`,
+  price: `140`,
+  rating: 3.1,
+  brand: `nike`,
+  description: `best shoes ever`,
+  colors: [`red`, `blue`],
+  stock: 10,
+  sizes: [7, 8, 9],
+  discount: 5,
+  onSale: false
+};
 
-shoesData=[nike1,nike2,nike3,puma1,puma2,nike1,nike2,nike3,puma1,puma2];
+const nike6 = {
+  name: `Nike shoes `,
+  imageUrl: `img/nike/running/nike-running%20(2).jpeg`,
+  productCode: `WDDM-117`,
+  altname: `nikeshoe3`,
+  category: `casual`,
+  price: `140`,
+  rating: 3.1,
+  brand: `nike`,
+  description: `best shoes ever`,
+  colors: [`red`, `blue`],
+  stock: 10,
+  sizes: [7, 8, 9],
+  discount: 5,
+  onSale: false
+};
 
+const nike7 = {
+  name: `Nike shoes `,
+  imageUrl: `img/nike/running/nike-running%20(3).jpeg`,
+  productCode: `WDDM-117`,
+  altname: `nikeshoe3`,
+  category: `casual`,
+  price: `140`,
+  rating: 3.1,
+  brand: `nike`,
+  description: `best shoes ever`,
+  colors: [`red`, `blue`],
+  stock: 10,
+  sizes: [7, 8, 9],
+  discount: 5,
+  onSale: false
+};
+
+const nike8 = {
+  name: `Nike shoes `,
+  imageUrl: `img/nike/sports/nike-sports%20(1).jpeg`,
+  productCode: `NS100`,
+  altname: `nikeshoe3`,
+  category: `sports`,
+  price: `140`,
+  rating: 3.1,
+  brand: `nike`,
+  description: `best shoes ever`,
+  colors: [`red`, `blue`],
+  stock: 10,
+  sizes: [7, 8, 9],
+  discount: 5,
+  onSale: false
+};
+
+const nike9 = {
+  name: `Nike shoes `,
+  imageUrl: `img/nike/sports/nike-sports%20(2).jpeg`,
+  productCode: `NS101`,
+  altname: `nikeshoe3`,
+  category: `sports`,
+  price: `160`,
+  rating: 4.1,
+  brand: `nike`,
+  description: `best shoes ever`,
+  colors: [`red`, `blue`],
+  stock: 10,
+  sizes: [7, 8, 9],
+  discount: 5,
+  onSale: false
+};
+
+const nike10 = {
+  name: `Nike shoes `,
+  imageUrl: `img/nike/sports/nike-sports%20(3).jpeg`,
+  productCode: `NS102`,
+  altname: `nikeshoe3`,
+  category: `sports`,
+  price: `160`,
+  rating: 4.1,
+  brand: `nike`,
+  description: `best shoes ever`,
+  colors: [`red`, `blue`],
+  stock: 10,
+  sizes: [7, 8, 9],
+  discount: 5,
+  onSale: false
+};
+
+const nike11 = {
+  name: `Nike shoes `,
+  imageUrl: `img/nike/sports/nike-sports%20(4).jpeg`,
+  productCode: `NS103`,
+  altname: `nikeshoe3`,
+  category: `sports`,
+  price: `160`,
+  rating: 4.1,
+  brand: `nike`,
+  description: `best shoes ever`,
+  colors: [`red`, `blue`],
+  stock: 10,
+  sizes: [7, 8, 9],
+  discount: 5,
+  onSale: false
+};
+
+const nike = [
+  nike1,
+  nike2,
+  nike3,
+  nike4,
+  nike5,
+  nike6,
+  nike7,
+  nike8,
+  nike9,
+  nike10,
+  nike11
+];
+
+const puma1 = {
+  name: `puma shoes 1`,
+  imageUrl: `img/puma/casual/puma-casual%20(1).jpeg`,
+  altname: `pumashoes1`,
+  category: `casual`,
+  productCode: `PUMA100`,
+  price: `1200`,
+  rating: 2,
+  brand: `puma`,
+  description: `best shoes ever`,
+  colors: [`red`, `blue`],
+  stock: 10,
+  sizes: [7, 8, 9],
+  discount: 5,
+  onSale: true
+};
+
+const puma2 = {
+  name: `puma shoes 2`,
+  imageUrl: `img/puma/casual/puma-casual%20(2).jpeg`,
+  altname: `pumashoes1`,
+  category: `casual`,
+  productCode: `PUMA101`,
+  price: `1200`,
+  rating: 2,
+  brand: `puma`,
+  description: `best shoes ever`,
+  colors: [`red`, `blue`],
+  stock: 10,
+  sizes: [7, 8, 9],
+  discount: 5,
+  onSale: true
+};
+
+const puma3 = {
+  name: `puma shoes 3`,
+  imageUrl: `img/puma/casual/puma-casual%20(3).jpeg`,
+  altname: `pumashoes3`,
+  category: `casual`,
+  productCode: `PUMA102`,
+  price: `1200`,
+  rating: 2,
+  brand: `puma`,
+  description: `best shoes ever`,
+  colors: [`red`, `blue`],
+  stock: 10,
+  sizes: [7, 8, 9],
+  discount: 5,
+  onSale: true
+};
+
+const puma4 = {
+  name: `puma shoes 4`,
+  imageUrl: `img/puma/casual/puma-casual%20(4).jpeg`,
+  altname: `pumashoes3`,
+  category: `casual`,
+  productCode: `PUMA103`,
+  price: `1200`,
+  rating: 2,
+  brand: `puma`,
+  description: `best shoes ever`,
+  colors: [`red`, `blue`],
+  stock: 10,
+  sizes: [7, 8, 9],
+  discount: 5,
+  onSale: true
+};
+
+const puma5 = {
+  name: `puma shoes 5`,
+  imageUrl: `img/puma/running/puma-running%20(1).jpeg`,
+  altname: `pumashoes3`,
+  category: `running`,
+  productCode: `PUMA104`,
+  price: `1200`,
+  rating: 2,
+  brand: `puma`,
+  description: `best shoes ever`,
+  colors: [`red`, `blue`],
+  stock: 10,
+  sizes: [7, 8, 9],
+  discount: 5,
+  onSale: true
+};
+
+const puma6 = {
+  name: `puma shoes 6`,
+  imageUrl: `img/puma/running/puma-running%20(2).jpeg`,
+  altname: `pumashoes3`,
+  category: `running`,
+  productCode: `PUMA105`,
+  price: `1200`,
+  rating: 2,
+  brand: `puma`,
+  description: `best shoes ever`,
+  colors: [`red`, `blue`],
+  stock: 10,
+  sizes: [7, 8, 9],
+  discount: 5,
+  onSale: true
+};
+
+const puma7 = {
+  name: `puma shoes 7`,
+  imageUrl: `img/puma/running/puma-running%20(3).jpeg`,
+  altname: `pumashoes3`,
+  category: `running`,
+  productCode: `PUMA106`,
+  price: `1200`,
+  rating: 2,
+  brand: `puma`,
+  description: `best shoes ever`,
+  colors: [`red`, `blue`],
+  stock: 10,
+  sizes: [7, 8, 9],
+  discount: 5,
+  onSale: true
+};
+
+const puma8 = {
+  name: `puma shoes 7`,
+  imageUrl: `img/puma/running/puma-running%20(4).jpeg`,
+  altname: `pumashoes3`,
+  category: `running`,
+  productCode: `PUMA107`,
+  price: `1200`,
+  rating: 2,
+  brand: `puma`,
+  description: `best shoes ever`,
+  colors: [`red`, `blue`],
+  stock: 10,
+  sizes: [7, 8, 9],
+  discount: 5,
+  onSale: true
+};
+
+const puma9 = {
+  name: `puma shoes 7`,
+  imageUrl: `img/puma/running/puma-running%20(4).jpeg`,
+  altname: `pumashoes3`,
+  category: `running`,
+  productCode: `PUMA108`,
+  price: `1200`,
+  rating: 2,
+  brand: `puma`,
+  description: `best shoes ever`,
+  colors: [`red`, `blue`],
+  stock: 10,
+  sizes: [7, 8, 9],
+  discount: 5,
+  onSale: true
+};
+
+const puma10 = {
+  name: `puma shoes 7`,
+  imageUrl: `img/puma/running/puma-running%20(5).jpeg`,
+  altname: `pumashoes3`,
+  category: `running`,
+  productCode: `PUMA109`,
+  price: `1200`,
+  rating: 2,
+  brand: `puma`,
+  description: `best shoes ever`,
+  colors: [`red`, `blue`],
+  stock: 10,
+  sizes: [7, 8, 9],
+  discount: 5,
+  onSale: true
+};
+
+const puma11 = {
+  name: `puma shoes 7`,
+  imageUrl: `img/puma/sports/puma-sports%20(1).jpeg`,
+  altname: `pumashoes3`,
+  category: `sports`,
+  productCode: `PUMA110`,
+  price: `1200`,
+  rating: 2,
+  brand: `puma`,
+  description: `best shoes ever`,
+  colors: [`red`, `blue`],
+  stock: 10,
+  sizes: [7, 8, 9],
+  discount: 5,
+  onSale: true
+};
+
+const puma12 = {
+  name: `puma shoes 7`,
+  imageUrl: `img/puma/sports/puma-sports%20(2).jpeg`,
+  altname: `pumashoes3`,
+  category: `sports`,
+  productCode: `PUMA111`,
+  price: `1200`,
+  rating: 2,
+  brand: `puma`,
+  description: `best shoes ever`,
+  colors: [`red`, `blue`],
+  stock: 10,
+  sizes: [7, 8, 9],
+  discount: 5,
+  onSale: true
+};
+
+const puma13 = {
+  name: `puma shoes 7`,
+  imageUrl: `img/puma/sports/puma-sports%20(3).jpeg`,
+  altname: `pumashoes3`,
+  category: `sports`,
+  productCode: `PUMA110`,
+  price: `1200`,
+  rating: 2,
+  brand: `puma`,
+  description: `best shoes ever`,
+  colors: [`red`, `blue`],
+  stock: 10,
+  sizes: [7, 8, 9],
+  discount: 5,
+  onSale: true
+};
+const puma = [
+  puma1,
+  puma2,
+  puma3,
+  puma4,
+  puma5,
+  puma6,
+  puma7,
+  puma8,
+  puma9,
+  puma10,
+  puma11,
+  puma12,
+  puma13
+];
+
+const shoesData = nike.concat(puma);
 
 //functions
-function getShoesAsHtml(shoesData){
-    return `
+function getShoesAsHtml(shoesData) {
+  return `
     <section class="productsCard">
     <section class="container">
-    <img src="${shoesData.imageUrl}" alt="${shoesData.altname}" style="width:100%">
-    <button type="button" class="heartBtn"><span class="material-icons">favorite</span></button>
+    <img src="${shoesData.imageUrl}" alt="${shoesData.altname}" style="width:100%;height:50%;">
+    <button type="button" class="heartBtn" id="heartBtn"><span class="material-icons">favorite</span></button>
     </section>
     <h1><strong>${shoesData.name}</strong></h1>
         <ul class="productCardList">
@@ -112,24 +452,34 @@ function getShoesAsHtml(shoesData){
           <li class="productCardListItem"><h3>Price:$${shoesData.price}</h3></li>
           <li class="productCardListItem"><p>Average rating:${shoesData.rating}<p></li>
         </ul>
-        <button type="button" class="productCardButton" id="${shoesData.productCode}"><span class="material-icons">add_shopping_cart</span> Add to Cart</button>
-        
+        <button type="button" class="productCardButton" id="addtocart"><span class="material-icons">add_shopping_cart</span> Add to Cart</button> 
         </section>`;
-    
 }
 
-document.getElementById(`app`).innerHTML=shoesData.map(getShoesAsHtml).join('\n');
+document.getElementById(`app`).innerHTML = shoesData
+  .map(getShoesAsHtml)
+  .join("\n");
 
+function addToCart() {
+  alert("in function");
+  document.getElementById(`addtocart`).style.backgroundColor = `red`;
+  document.getElementById(`addtocart`).innerHTML = `Remove from Cart`;
+}
 
-function addToCart(){
-    alert("in function");
-    document.getElementById(`${shoesData.productCode}`).style.backgroundColor=`red`;
-    document.getElementById(`${shoesData.productCode}`).innerHTML=`Added To Cart!`;
-} 
+function addToWishlist() {
+  alert("in add to wishlist function");
+  document.getElementById(`heartBtn`).style.color = `red`;
+}
 
+const PriceHighToLowArray = shoesData.sort(function(a, b) {
+  return a.price > b.price;
+});
+const PriceHighToLowArray = shoesData.slice(0);
 
+PriceHighToLowArray.sort((a, b) => (a.price > b.price ? 1 : -1));
+
+alert(PriceHighToLowArray);
 
 //execution
-document.getElementById(`${shoesData.productCode}`).addEventListener("click",addToCart);
-
-
+document.getElementById(`addtocart`).addEventListener("click", addToCart);
+document.getElementById(`heartBtn`).addEventListener("click", addToWishlist);
