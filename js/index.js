e585b718ae38710a7e2cb840b8dbaa1082b78650
sashidhar-meesala <sashidharklu@gmail.com>
2019-12-08 23:30:08 -0800

    added logo,mobile resposive

--- js/index.js
+++ js/index.js
@@ -512,10 +512,12 @@ const addItemToCart = pID => {
 
 function LoadFilterMenu(){
   const x = document.getElementById("filtermenu");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
+  if (x.style.display === "block") {
     x.style.display = "none";
+    
+  } else {
+    x.style.display = "block";
+    //document.getElementById("layoutTheme").classList.toggle('layoutthemex');
   }
 }
 //filter functions
